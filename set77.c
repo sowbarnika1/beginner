321a6c8de9e11be584c54cb108d44f24d7bfa514
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-02 22:11:58 +0530

    Create set77.c

---
+++ set77.c
@@ -0,0 +1,15 @@
+int main()
+{
+     int n,i;
+     scanf("%d",&n);
+         if(n%7==0)
+         {
+             printf("yes");
+         }
+         else
+         {
+             printf("no");
+         }
+
+return 0;
+}

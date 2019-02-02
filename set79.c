8b446dd0ff4794d31ce0b82f0b80b8d8b5c4c894
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-02 22:16:44 +0530

    Create set79.c

---
+++ set79.c
@@ -0,0 +1,16 @@
+int main()
+{
+     int n,k,d;
+     scanf("%d %d",&n,&k);
+     d=n-k;
+         if(d%2==0)
+         {
+             printf("even");
+         }
+         else
+         {
+             printf("odd");
+         }
+
+return 0;
+}

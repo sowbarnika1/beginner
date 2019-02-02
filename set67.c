311a000b39b74a41b41cedbfc95313e110d491ee
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-02 22:07:16 +0530

    Create set67.c

---
+++ set67.c
@@ -0,0 +1,17 @@
+int main()
+{
+     int n,i,flag=0;
+     scanf("%d",&n);
+     if(n>='0' && n<='10000')
+     {
+     for(i=n;;i++)
+     {
+         if(i%10==0)
+         {
+             printf("%d",i);
+             break;
+         }
+}
+}
+return 0;
+}

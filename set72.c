1bcf2ec69919d62c0314a3c0d32de14d5d51f197
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-02 20:55:32 +0530

    Create set72.c

---
+++ set72.c
@@ -0,0 +1,23 @@
+int main()
+{
+    char a[50];
+    int i,n=0,c=0;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+     n++; 
+     if(a[i]=='0' || a[i]=='1')
+     {
+         c++;
+     }
+    }
+    if(c==n)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+return 0;
+}

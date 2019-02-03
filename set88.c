86657feb14c85f9dbdda4fafcd0ffac6eabd1e6f
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-03 13:56:54 +0530

    Create set88.c

---
+++ set88.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int n;
+    scanf("%d",&n);
+        if(n%13==0)
+        {
+        printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+   return 0;
+}

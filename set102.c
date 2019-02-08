402f3e0d1003cfec6288af01bae64666e9bf7406
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-08 21:38:34 +0530

    Create set102.c

---
+++ set102.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int n,a[100],s=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    printf("%d",s);
+return 0;
+}

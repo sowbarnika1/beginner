13c5b80c983234558055f63f59005343709c0e80
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-10 13:30:42 +0530

    Create set97.c

---
+++ set97.c
@@ -0,0 +1,14 @@
+int main()
+  {
+      int n,m,i,gcd;
+      scanf("%d %d",&n,&m);
+      for(i=1;i<=n && i<=m;i++)
+      {
+          if(n%i==0 && m%i==0)
+          {
+              gcd=i;
+          }
+      }
+      printf("%d",gcd);
+      return 0;
+  }
